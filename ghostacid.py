578504931c8532308e24953ebf58a07fabfb2de7
Kyle Wukawitz <wukawitz@gmail.com>
2017-07-17 21:04:27 -0400

    Initial commit of code

---
+++ ghostacid.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ga.config.banner import banner_text
+
+def main():
+    """
+    Main entry into the application
+    """
+    print banner_text
+
+if __name__ == "__main__":
+
+    main()
